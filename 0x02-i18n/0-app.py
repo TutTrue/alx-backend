5db99fbe8a4be3218419d2d3d464a354c400bb35
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-02-06 23:39:53 +0200

    type annotation

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -3,11 +3,12 @@
 
 
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
+def index() -> str:
     """ Returns the index page """
     return render_template('0-index.html')
 
