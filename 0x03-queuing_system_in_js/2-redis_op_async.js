2e687a17bb404847d470377a27990c014d2347e5
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-03-04 11:21:11 +0200

    async/await with redis

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,27 @@
+import { createClient, print } from 'redis';
+import { promisify } from 'util';
+
+const client = createClient();
+const get = promisify(client.get).bind(client);
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, print);
+}
+
+async function displaySchoolValue(schoolName) {
+  const value = await get(schoolName);
+  console.log(value);
+}
+(async () => {
+  await displaySchoolValue('Holberton');
+  setNewSchool('HolbertonSanFrancisco', '100');
+  await displaySchoolValue('HolbertonSanFrancisco');
+})();
